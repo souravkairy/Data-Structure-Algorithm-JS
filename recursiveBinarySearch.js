658d6bfb3231f8a4f279e997c85827d341ab55fa
souravkairy <48748661+souravkairy@users.noreply.github.com>
2022-10-27 09:18:31 +0600

    Create recursiveBinarySearch.js

---
+++ recursiveBinarySearch.js
@@ -0,0 +1,23 @@
+function rbs(arr, target) {
+    return search(arr, target, 0, arr.length - 1)
+}
+function search(arr, target, leftIndex, rightIndex) {
+    if (leftIndex > rightIndex) {
+        return -1;
+    }
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+
+    if (target === arr[middleIndex]) {
+        return middleIndex
+    }
+    if (target < arr[middleIndex]) {
+        return search(arr, target, leftIndex, middleIndex - 1)
+    }
+    else {
+        return search(arr, target, middleIndex + 1, rightIndex)
+    }
+}
+
+console.log(rbs([1, 2, 3, 4, 5], 4))
+
+//Big(O) = O(logn)
